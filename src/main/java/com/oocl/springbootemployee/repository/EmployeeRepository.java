385bf1babae9f04f9f4b3f485d562f9f080ca3c8
HEAL <HEAL@OOCL.COM>
2024-11-26 15:44:02 +0800

    [SpringBoot][JPA] refactor: refactor findAllWithGender function

--- src/main/java/com/oocl/springbootemployee/repository/EmployeeRepository.java
+++ src/main/java/com/oocl/springbootemployee/repository/EmployeeRepository.java
@@ -1,10 +1,13 @@
 package com.oocl.springbootemployee.repository;
 
 import com.oocl.springbootemployee.model.Employee;
+import com.oocl.springbootemployee.model.Gender;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
+    List<Employee> findByGender(Gender gender);
 }
