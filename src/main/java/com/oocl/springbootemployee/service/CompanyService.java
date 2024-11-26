f866424cff32bc2e224c8a862182286c46af7db3
HEAL <HEAL@OOCL.COM>
2024-11-26 21:12:01 +0800

    [SpringBoot][JPA] refactor: pass should_return_updated_company_when_update_with_id_and_data test using Spring Data JPA

--- src/main/java/com/oocl/springbootemployee/service/CompanyService.java
+++ src/main/java/com/oocl/springbootemployee/service/CompanyService.java
@@ -50,13 +50,14 @@ public class CompanyService {
     }
 
     public Company update(Integer id, Company company) {
-        final var companyNeedToUpdate = companyInMemoryRepository
-                .findById(id);
+        final var companyNeedToUpdate = companyRepository
+                .findById(id)
+                .orElseThrow(() -> new CompanyNotFoundException("No such company"));
 
         var nameToUpdate = company.getName() == null ? companyNeedToUpdate.getName() : company.getName();
         var employeesToUpdate = company.getEmployees() == null ? companyNeedToUpdate.getEmployees() : company.getEmployees();
 
         final var companyToUpdate = new Company(id,nameToUpdate,employeesToUpdate);
-        return companyInMemoryRepository.updateCompany(id, companyToUpdate);
+        return companyRepository.save(companyToUpdate);
     }
 }
